a7a465f78a4f5ecc3a960a691bab489a774994e6
Trevor Cash <tjcashit@gmail.com>
2022-03-28 15:47:19 -0500

    Added video to webpage+ added hover on video

---
+++ script.js
@@ -0,0 +1,11 @@
+console.log("page loaded...");
+
+var video = document.getElementById("mainVideo");
+
+function over(element) {
+    video.play();
+}
+
+function out(element) {
+    video.pause();
+}
